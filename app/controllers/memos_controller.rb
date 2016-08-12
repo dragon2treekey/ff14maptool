b8d8512bb3288bb9a6065ad1f669eeaa1dcfd730
unknown <>
2016-08-12 20:05:14 +0900

    グループ毎の座標の表示を実装。

--- app/controllers/memos_controller.rb
+++ app/controllers/memos_controller.rb
@@ -1,18 +1,18 @@
 class MemosController < ApplicationController
   def g8map_kanri
-    @memos = Memo.all.order(:text)
+    @memos = Memo.where(group_id: current_user.group_id).order(:text)
     @memo = Memo.new
     @memo.text = "画像を選択してください"
   end
 
   def create
     Memo.create(text: memo_params[:text], name: memo_params[:name], group_id: current_user.group_id)
-    @memos = Memo.all.order(:text)
+    @memos = Memo.where(group_id: current_user.group_id).order(:text)
   end
 
   def destroy
     Memo.find(params[:id]).destroy
-    @memos = Memo.all.order(:text)
+    @memos = Memo.where(group_id: current_user.group_id).order(:text)
   end
 
   private

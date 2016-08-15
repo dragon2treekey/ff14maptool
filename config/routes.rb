ea26c4c3b081ea184e41da6d61c85aa7d96e6c53
unknown <>
2016-08-15 19:22:30 +0900

    非同期更新機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   resources :ff14map_index, only: :index
   get 'g8map' => 'memos#g8map_kanri'
   get 'group' => 'groups#group'
-  resources :memos, only: [:create, :destroy]
+  resources :memos, only: [:create, :destroy, :update]
 end

8442db144234c883b7cd4c7173ac81e92c5018dc
unknown <>
2016-08-19 13:25:52 +0900

    ユーザー情報更新機能、SCSS修正

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -2,5 +2,6 @@ class GroupsController < ApplicationController
   def group
     @groups = Group.all.order(:key)
     @group = Group.new
+    @mygroups = Group.where(id: current_user.group_id)
   end
 end
